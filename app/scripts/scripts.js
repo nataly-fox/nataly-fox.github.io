8fd8a460f4e8b5bbedd60af2033c5a7d44e0cda2
ekudryashov <evgenykud@gmail.com>
2018-08-17 07:29:52 +0400

    t2

---
+++ app/scripts/scripts.js
@@ -0,0 +1,24 @@
+'use strict';
+
+agCookie.create('example-cookie', true, 1);
+
+var readValue = agCookie.read('example-cookie');
+
+console.log(readValue);
+
+agCookie.erase('example-cookie');
+
+function markoFunkcija(args) {
+    console.log(args);
+
+    return true;
+}
+'use strict';
+
+console.log('I have entered this file.');
+console.log('This is crystal clear evidence that this works.');
+
+var calculation = 200 + 223;
+
+console.log('Sanity Check: 200 + 223 = ' + calculation);
+console.log('bye.');
